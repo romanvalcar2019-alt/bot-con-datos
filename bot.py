c202edaabf7eb8a1fc516ea33d4ccac6e328597f
romanvalcar2019-alt <romanvalcar2019@gmail.com>
2026-03-21 18:36:43 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,22 +9,43 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+
+@bot.event
+async def on_message(message):
+    if message.content.startswith('!deleteme'):
+        msg = await message.channel.send('I will delete myself now...')
+        await msg.delete()
+
+        await message.channel.send(
+            'Goodbye in 3 seconds...',
+            delete_after=3.0
+        )
+
+    await bot.process_commands(message)
+
+
+@bot.event
+async def on_message_delete(message):
+    msg = f'{message.author} ha eliminado el mensaje: {message.content}'
+    await message.channel.send(msg)
+
 
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Hola, soy un bot {bot.user}!')
 
+
 @bot.command()
-async def heh(ctx, count_heh = 5):
+async def heh(ctx, count_heh=5):
     await ctx.send("he" * count_heh)
 
 
 @bot.command(description='For when you wanna settle the score some other way')
 async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
     await ctx.send(random.choice(choices))
-
 
 
 bot.run("TOKEN")
